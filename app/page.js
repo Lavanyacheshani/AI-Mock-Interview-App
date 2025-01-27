4ce01312cac6b35cd2995f89e3ea4ffa7d7ee49f
Lavanyacheshani <143724777+Lavanyacheshani@users.noreply.github.com>
2025-01-27 18:00:20 +0530

    add a button

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Hi lavanya</h2>
+      <Button>Subcribe</Button>
 
     </div>
   );
